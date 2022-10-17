79fedde5fd15647e6756f768db96578938215bec
Chu Chen <chu.chen@salesforce.com>
2022-10-17 14:14:00 -0400

    remove unused functions, remove git card field

--- cartridges/plugin_reorder_demo/cartridge/controllers/Account.js
+++ cartridges/plugin_reorder_demo/cartridge/controllers/Account.js
@@ -3,39 +3,6 @@
 var server = require('server');
 server.extend(module.superModule);
 
-server.append('Show', function (req, res, next) {
-    var URLUtils = require('dw/web/URLUtils');
-    var AccountModel = require('*/cartridge/models/account');
-    var configurationHelper = require('*/cartridge/scripts/configurationHelper');
-
-    configurationHelper.appendConfiguration(res);
-    var viewData = res.getViewData();
-
-    if (viewData.commercePaymentsConfiguration.multiStepCheckoutEnabled) {
-        var paymentMethods = AccountModel.getCustomerPaymentMethods(req.currentCustomer.raw);
-
-        viewData.viewSavedPaymentsUrl = URLUtils.url('PaymentInstruments-ListPaymentMethods').toString();
-        viewData.addPaymentUrl = null;
-        if (paymentMethods.empty) {
-            viewData.payment = null;
-        } else {
-            viewData.payment = paymentMethods[0];
-        }
-    }
-
-    res.setViewData(viewData);
-    next();
-});
-
-server.get('CreateOffSessionOrder', server.middleware.https, function (req, res, next) {
-    var URLUtils = require('dw/web/URLUtils');
-    var offSessionOrders = require('~/cartridge/scripts/offSessionOrders');
-
-    var order = offSessionOrders.createOrder(req.currentCustomer.raw);
-    res.redirect(URLUtils.url('Order-Details', 'orderID', order.orderNo));
-    next();
-});
-
 server.get('Reorder', function (req, res, next) {
     var URLUtils = require('dw/web/URLUtils');
     var offSessionOrders = require('~/cartridge/scripts/offSessionOrders');
